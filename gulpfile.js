216336b4541b094da7918ef0973aee2977b2522f
Brandon Spada <brandonsgh123127@gmail.com>
2020-04-28 14:14:40 -0400

    Add html page

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const {src,dest} = require(`gulp`);
+const {watch} = require(`gulp`);
+
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+const htmlCompress= require(`gulp-htmlmin`);
+const cssCompress = require(`gulp-clean-css`);
+
+let compressHTML = () => {
+    return src(`html/*.html`).pipe(htmlCompress({
+        collapseWhitespace: true})).pipe(dest(`temp/`))
+};
+let compressCSS = () => {
+    return src(`css/*.css`).pipe(cssCompress({
+        collapseWhitespace: true})).pipe(dest(`temp/css/`))
+};
+
+let sync = () =>{
+    browserSync({
+        server: {
+            baseDir: `./temp`
+        }
+    });
+
+    watch([
+        `./html/*.html`,
+        `./js/**/*.js`,
+        `./css/*.css`
+    ]).on(`change`,compressHTML).on(`change`,compressCSS)
+        .on(`change`,reload);};
+
+exports.default = compressHTML;
+exports.compressHTML = compressHTML;
+exports.compressCSS = compressCSS;
+exports.sync = sync;
